970aeefb8c326578a68f6b72328dea5945b44b03
Stepan Busygin <stepan.busygin1@gmail.com>
2026-03-22 22:15:28 +0500

    Начал создавать систему классов, инициализировал тесты, добавил svg

---
+++ YetAnotherStrategyGame/Domain/Game.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YetAnotherStrategyGame.Domain
+{
+    enum Team
+    {
+        First,
+        Second
+    }
+
+    class Game
+    {
+        public Player FirstPlayer { get; set; }
+        public Player SecondPlayer { get; set; }
+    }
+
+    class Player
+    {
+        public string Name { get; }
+        public Team Team { get; }
+    }
+
+    class Field
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+
+
+    }
+
+    class Attack
+    {
+
+    }
+
+    interface IUnit
+    {
+
+    }
+
+    class Warrior : IUnit
+    {
+
+    }
+
+    class Crossbowman : IUnit
+    {
+
+    }
+
+    class Cannon : IUnit
+    {
+
+    }
+
+    interface IBuilding
+    {
+
+    }
+
+    class House : IBuilding
+    {
+
+    }
+
+    class Farm : IBuilding
+    {
+
+    }
+
+    class Mine : IBuilding
+    {
+
+    }
+
+    class Barracks : IBuilding
+    {
+
+    }
+
+    class CrossbowWorkshop : IBuilding
+    {
+
+    }
+
+    class CannonFactory : IBuilding
+    {
+
+    }
+}
---
+++ YetAnotherStrategyGame/Domain/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace YetAnotherStrategyGame.Domain
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+}
